8d142ea66791bcbd7f9f7ed224e11bd80cbe6dc7
unknown <farcwanted222@gmail.com>
2023-09-18 10:24:45 -0300

    responsive-options-navigation

--- src/components/OptionsNavigation/index.tsx
+++ src/components/OptionsNavigation/index.tsx
@@ -3,28 +3,28 @@ import { Flex, Image } from "@chakra-ui/react";
 
 const OptionsNavigation: React.FC = () => {
     return (
-        <Flex justifyContent="space-around" my={10}>
+        <Flex justifyContent="space-around" my={10} flexWrap="wrap" gap={5}>
             <Image
                 cursor="pointer"
-                w={52}
+                w={{ base: 36, md: 52 }}
                 src="/iconeinicio_frete.svg"
                 alt="Frete"
             />
             <Image
                 cursor="pointer"
-                w={52}
+                w={{ base: 36, md: 52 }}
                 src="/iconeinicio_suporte.svg"
                 alt="suporte"
             />
             <Image
                 cursor="pointer"
-                w={52}
+                w={{ base: 36, md: 52 }}
                 src="/iconeinicio_revendedor.svg"
                 alt="revendedor"
             />
             <Image
                 cursor="pointer"
-                w={52}
+                w={{ base: 36, md: 52 }}
                 src="/iconeinicio_parcele.svg"
                 alt="parcele"
             />
